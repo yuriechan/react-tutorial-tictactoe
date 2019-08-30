3b5b4c8b08175afa2412edefbb3dd3a46163d849
Yurie <cutecupcakeandcute@gmail.com>
2019-08-30 18:49:33 +0900

    rewrite Square class component into functional component

--- src/index.js
+++ src/index.js
@@ -2,21 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button
-        className="square"
-        // `onClick` prop tells React to set up a click event listener
-        // The function inside will be invoked, when `button` is clicked
-        onClick={() => {
-          this.props.onClick();
-        }}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button
+      className="square"
+      // `onClick` prop tells React to set up a click event listener
+      // The function inside will be invoked, when `button` is clicked
+      onClick={() => {
+        props.onClick();
+      }}
+    >
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
